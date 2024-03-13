dfbfae9767cafbcd09a7d9103f08f561150acc42
jhonatabezerra <jhonata.empresa+github@gmail.com>
2024-03-13 20:36:54 -0300

    Initial commit

---
+++ src/Atomikku.Models.Extension/Core/ITransactions.cs
@@ -0,0 +1,10 @@
+﻿namespace Atomikku.Models.Extension;
+
+public interface ITransactions
+{
+    Task<LightNovel> GetBookData(LightNovel lightNovel);
+
+    Task<LightNovel> GetChapters(LightNovel lightNovel);
+
+    Task<LightNovelToSearch> GetSearchBooks(LightNovelToSearch lightNovel);
+}
---
+++ src/Atomikku.Models.Extension/Core/Transactions.cs
@@ -0,0 +1,32 @@
+﻿namespace Atomikku.Models.Extension;
+
+public class Transactions : ITransactions
+{
+    private readonly ITransactions _transactions;
+
+    public Transactions(ITransactions transactions)
+    {
+        _transactions = transactions;
+    }
+
+    /// <summary>
+    /// Search books according the <see cref="LightNovelToSearch.Tittle"/>.
+    /// </summary>
+    /// <param name="lightNovel">Used to get the tittle.</param>
+    /// <returns>The collection of books.</returns>
+    public async Task<LightNovelToSearch> GetSearchBooks(LightNovelToSearch lightNovel) => await _transactions.GetSearchBooks(lightNovel);
+
+    /// <summary>
+    /// Get the book data.
+    /// </summary>
+    /// <param name="lightNovel">Used to update the information.</param>
+    /// <returns>The <see cref="LightNovel"/> with data.</returns>
+    public async Task<LightNovel> GetBookData(LightNovel lightNovel) => await _transactions.GetBookData(lightNovel);
+
+    /// <summary>
+    /// Get all chapters from the book.
+    /// </summary>
+    /// <param name="lightNovel">Used to update the information.</param>
+    /// <returns>The <see cref="LightNovel"/> with the chapters.</returns>
+    public async Task<LightNovel> GetChapters(LightNovel lightNovel) => await _transactions.GetChapters(lightNovel);
+}
